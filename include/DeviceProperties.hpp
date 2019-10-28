4e8cb6d39823a5667e8849d1cf5d6fcfb5b90b50
terminal29 <20899505+terminal29@users.noreply.github.com>
2019-10-28 17:32:30 +1000

    Added docs to DeviceProperties

--- include/DeviceProperties.hpp
+++ include/DeviceProperties.hpp
@@ -11,8 +11,18 @@ struct DeviceProperties {
     using PropName = std::string;
     using PropVal = std::string;
 
+    /// <summary>
+    /// Sets a property
+    /// </summary>
+    /// <param name="key">Property key</param>
+    /// <param name="value">Property value</param>
     void setProperty(PropKey key, PropVal value);
 
+    /// <summary>
+    /// Gets a property
+    /// </summary>
+    /// <param name="key">Property key</param>
+    /// <returns>Property value, or nullopt if not set</returns>
     std::optional<PropVal> getProperty(PropKey key);
 
     const static std::map<PropKey, PropName> m_propertyKeys;
