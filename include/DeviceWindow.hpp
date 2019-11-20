b6891d9ff97dbf897ae685e69753736b28fe1104
terminal29 <20899505+terminal29@users.noreply.github.com>
2019-11-20 14:28:43 +1000

    Added header

---
+++ include/DeviceWindow.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <memory>
+
+#include <imgui.h>
+#include <OpenVRDevice.hpp>
+
+class DeviceWindow {
+public:
+    DeviceWindow(std::shared_ptr<OpenVRDevice> device);
+
+    void show();
+
+    void hide();
+
+    void draw();
+
+    virtual ~DeviceWindow() = default;
+private:
+    std::shared_ptr<OpenVRDevice> m_device;
+    bool m_isVisible;
+    std::string m_windowName;
+};
